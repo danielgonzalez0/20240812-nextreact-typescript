d7012963e7b8e8994a4e4c0b29f1b88b7250fb32
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-08-13 07:20:54 +0200

    (feat): exo 1 partie 2 et 3 ts

--- src/exercices/1.tsx
+++ src/exercices/1.tsx
@@ -1,34 +1,101 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
-import clsx from "clsx";
-import {ComponentPropsWithoutRef, PropsWithChildren } from "react";
+import clsx from 'clsx';
+import { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
+import { calculateNextValue, calculateStatus } from '../lib/tictactoe/helpers';
 
 // 🦁 Supprime ce commentaire et définis correctement les types pour ce composant
 type SquareProps = {
-isWinningSquare?: boolean;
-}&ComponentPropsWithoutRef<"button">;
+  isWinningSquare?: boolean;
+} & ComponentPropsWithoutRef<'button'>;
 
-const Square = ({isWinningSquare, children, ...props}:PropsWithChildren<SquareProps>) => {
+const Square = ({
+  isWinningSquare,
+  children,
+  ...props
+}: PropsWithChildren<SquareProps>) => {
   // 🦁 Remplace ça par les props définies en haut
   return (
     <button
-      className={clsx("square", {
-        "winning-square": isWinningSquare, // 🦁 Remplace ça par la prop isWinningSquare
+      className={clsx('square', {
+        'winning-square': isWinningSquare, // 🦁 Remplace ça par la prop isWinningSquare
       })}
       {...props}
-      onClick={() => {console.log("Square clicked")}}
+      onClick={() => {
+        console.log('Square clicked');
+      }}
     >
       {children}
     </button>
   );
 };
 
+//component Board
+
+type SquareValue = 'X' | 'O' | null;
+type BoardProps = {
+  squares: SquareValue[];
+  onClick?: (index: number) => void;
+  winningSquares?: number[];
+};
+
+const Board = ({ squares, onClick, winningSquares }: BoardProps) => {
+  return (
+    <div className="game-board">
+      {squares.map((square, i) => (
+        <Square
+          key={`square-${i}`}
+          onClick={() => onClick?.(i)}
+          isWinningSquare={winningSquares?.includes(i)}
+        >
+          {square}
+        </Square>
+      ))}
+    </div>
+  );
+};
+
+// component GameInfo
+
+type GameInfoProps = {
+  status: string;
+};
+
+const GameInfo = ({ status }: GameInfoProps) => {
+  return (
+    <div className="game-info">
+      <p>{status}</p>
+    </div>
+  );
+};
+
+//component Game
+
+const getDefaultSquares = (): SquareValue[] => [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  'O',
+  null,
+  'X',
+];
+
 const Game = () => {
+  const squares = getDefaultSquares();
+  const nextPlayer = calculateNextValue(squares);
+  const status = calculateStatus(squares, nextPlayer);
+
   return (
     <div className="game">
-      <Square isWinningSquare={true}>X</Square>
-      <Square isWinningSquare={false}>X</Square>
-      <Square isWinningSquare={true}>O</Square>
+      <GameInfo status={status} />
+      <Board
+        squares={squares}
+        onClick={(i) => console.log(i)}
+        winningSquares={[6, 8]}
+      />
     </div>
   );
 };
